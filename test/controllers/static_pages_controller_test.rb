2832ef99321232b853237a828b4c8c8800128acb
Mayowa Pitan <mayowa.pitan@andela.com>
2016-09-17 23:18:33 +0100

    correct static_pages_controller specs to fix failing builds on circle ci

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe StaticPagesController do
   it "should get home" do
-    get root_path
+    get :home
 
     must_respond_with :success
     must_render_template :home
