4520b25a6b54f733d8ec93f8b3094b2a2d3deb79
Mayowa Pitan <mayowa.pitan@andela.com>
2016-09-18 23:45:05 +0100

    complete partial for rendering images and properly style the rendition

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  def picture_formatter(photo)
+    image_tag photo_url(photo), alt: "An image is supposed to be here"
+  end
+
+  def photo_url(photo)
+    "https://farm#{photo.farm}.staticflickr.com/#{photo.server}/#{photo.id}_#{photo.secret}.jpg"
+  end
 end
